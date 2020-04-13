f995f2fffd8bd50068de882276b54b36a45cde26
wanglei <cn_wlei@163.com>
2020-04-13 14:17:55 +0800

    1.微服务网关Gateway模块搭建 2.网关限流（一个客户端1秒内只能请求一次）

---
+++ changgou_gateway/changgou_gateway_system/src/main/java/com/changgou/system/GateWayApplication.java
@@ -0,0 +1,25 @@
+package com.changgou.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
+
+    //定义一个KeyResolver
+    @Bean
+    public KeyResolver ipKeyResolver() {
+        return exchange -> Mono.just(Objects.requireNonNull(exchange.getRequest().getRemoteAddress()).getHostName());
+    }
+}
