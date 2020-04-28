1a7d7deea41628a25b793a9b242997fef4e3bed6
wanglei <cn_wlei@163.com>
2020-04-28 17:26:18 +0800

    Springboot整合thymeleaf(一)

--- changgou_service/changgou_service_search/src/main/java/com/changgou/search/controller/SearchController.java
+++ changgou_service/changgou_service_search/src/main/java/com/changgou/search/controller/SearchController.java
@@ -2,18 +2,18 @@ package com.changgou.search.controller;
 
 import com.changgou.search.service.ESManagerService;
 import com.changgou.search.service.SearchService;
-import com.changgou.search.service.impl.ESManagerServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 import java.util.Set;
 
-@RestController
-@RequestMapping("/sku_search")
+@Controller
+@RequestMapping("/search")
 public class SearchController {
 
     @Autowired
@@ -21,6 +21,29 @@ public class SearchController {
 
     @Autowired
     private SearchService searchService;
+
+    /**
+     * 搜索页面   http://localhost:9009/search/list?keywords=手机&brand=三星&spec_颜色=粉色&
+     * 由于页面是thymeleaf 完成的 属于服务器内页面渲染 跳转页面
+     *
+     * @param searchMap
+     * @param model
+     * @return
+     * @throws Exception
+     */
+    @GetMapping("/list")
+    public String search(@RequestParam Map<String, String> searchMap, Model model) throws Exception {
+
+        //特殊符号处理
+        handlerSearchMap(searchMap);
+
+        //执行查询返回值
+        Map<String, Object> resultMap = searchService.search(searchMap);
+
+        model.addAttribute("searchMap", searchMap);
+        model.addAttribute("result", resultMap);
+        return "search";
+    }
 
     /**
      * 对搜索入参带有特殊符号进行处理
@@ -37,18 +60,5 @@ public class SearchController {
             }
         }
 
-    }
-
-    /**
-     * 全文检索
-     *
-     * @return
-     */
-    @GetMapping
-    public Map search(@RequestParam Map<String, String> paramMap) throws Exception {
-        //特殊符号处理
-        handlerSearchMap(paramMap);
-        Map resultMap = searchService.search(paramMap);
-        return resultMap;
     }
 }
