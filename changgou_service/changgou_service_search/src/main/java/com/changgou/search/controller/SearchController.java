b38e67ef54ce690cf98ee8f201fe1640ac97f1d2
wanglei <cn_wlei@163.com>
2020-05-06 14:51:00 +0800

    1.条件搜索实现 2.排序 分页

--- changgou_service/changgou_service_search/src/main/java/com/changgou/search/controller/SearchController.java
+++ changgou_service/changgou_service_search/src/main/java/com/changgou/search/controller/SearchController.java
@@ -1,5 +1,7 @@
 package com.changgou.search.controller;
 
+import com.changgou.entity.Page;
+import com.changgou.search.pojo.SkuInfo;
 import com.changgou.search.service.ESManagerService;
 import com.changgou.search.service.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,6 +44,36 @@ public class SearchController {
 
         model.addAttribute("searchMap", searchMap);
         model.addAttribute("result", resultMap);
+
+        //封装分页数据并返回
+        //1.总记录数
+        //2.当前页
+        //3.每页显示多少条
+        Page<SkuInfo> page = new Page<SkuInfo>(
+                Long.parseLong(String.valueOf(resultMap.get("total"))),
+                Integer.parseInt(String.valueOf(resultMap.get("pageNum"))),
+                Page.pageSize);
+        model.addAttribute("page", page);
+
+        //拼装url
+        StringBuilder url = new StringBuilder("/search/list");
+        if (searchMap != null && searchMap.size() > 0) {
+            //是由查询条件
+            url.append("?");
+            for (String paramKey : searchMap.keySet()) {
+                if (!"sortRule".equals(paramKey) && !"sortField".equals(paramKey) && !"pageNum".equals(paramKey)) {
+                    url.append(paramKey).append("=").append(searchMap.get(paramKey)).append("&");
+                }
+            }
+            //http://localhost:9009/search/list?keywords=手机&spec_网络制式=4G&
+            String urlString = url.toString();
+            //去除路径上的最后一个&
+            urlString = urlString.substring(0, urlString.length() - 1);
+            model.addAttribute("url", urlString);
+        } else {
+            model.addAttribute("url", url);
+        }
+
         return "search";
     }
 
